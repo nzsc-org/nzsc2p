932fedd8df13871c48e38dce08c6400226dbe9c6
Kyle Lin <kylejlin.us@gmail.com>
2018-06-06 19:22:57 -0700

    Reverse the logic if chooser is b

--- src/two_player_game.rs
+++ src/two_player_game.rs
@@ -44,124 +44,137 @@ impl NZSCTwoPlayerGame {
 
         let return_val = match &mut self.phase {
             &mut Phase::CharacterChoosing(ref mut a, ref mut b) => {
-                match chooser {
+                let tuple = match chooser {
                     WhichPlayer::PlayerA => {
-                        if let Ok(character) = Character::from_str(&choice[..]) {
-                            if a.character_streak.times == 3
-                                && a.character_streak.repeated_character == Some(character)
-                            {
-                                b.points += a.penalize(3);
-                                Ok(())
-                            } else {
-                                if let Some(b_character) = b.selected_character {
-                                    if character == b_character {
-                                        a.selected_character = None;
-                                        b.selected_character = None;
-                                        a.character_streak.add(character);
-                                        b.character_streak.add(character);
-                                        Ok(())
-                                    } else {
-                                        new_phase = Some(Phase::BoosterChoosing(
-                                            a.to_boosterless_player(character),
-                                            b.to_boosterless_player(b_character),
-                                        ));
-                                        Err(())
-                                    }
-                                } else {
-                                    a.selected_character = Some(character);
-                                    Ok(())
-                                }
-                            }
-                        } else {
-                            b.points += a.penalize(4);
-                            Ok(())
-                        }
+                        (a, b)
                     },
                     WhichPlayer::PlayerB => {
-                        // TODO
-                        Err(())
+                        (b, a)
                     },
+                };
+                let mut a = tuple.0;
+                let mut b = tuple.1;
+
+                if let Ok(character) = Character::from_str(&choice[..]) {
+                    if a.character_streak.times == 3
+                        && a.character_streak.repeated_character == Some(character)
+                    {
+                        b.points += a.penalize(3);
+                        Ok(())
+                    } else {
+                        if let Some(b_character) = b.selected_character {
+                            if character == b_character {
+                                a.selected_character = None;
+                                b.selected_character = None;
+                                a.character_streak.add(character);
+                                b.character_streak.add(character);
+                                Ok(())
+                            } else {
+                                new_phase = Some(Phase::BoosterChoosing(
+                                    a.to_boosterless_player(character),
+                                    b.to_boosterless_player(b_character),
+                                ));
+                                Err(())
+                            }
+                        } else {
+                            a.selected_character = Some(character);
+                            Ok(())
+                        }
+                    }
+                } else {
+                    b.points += a.penalize(4);
+                    Ok(())
                 }
             },
 
             &mut Phase::BoosterChoosing(ref mut a, ref mut b) => {
-                match chooser {
+                let tuple = match chooser {
                     WhichPlayer::PlayerA => {
-                        if let Ok(booster) = Booster::from_str(&choice[..]) {
-                            if let Some(b_booster) = b.selected_booster {
-                                new_phase = Some(Phase::MoveChoosing(
-                                    a.to_moveless_player(booster),
-                                    b.to_moveless_player(b_booster),
-                                ));
-                                Ok(())
-                            } else {
-                                a.selected_booster = Some(booster);
-                                Ok(())
-                            }
-                        } else {
-                            b.points += a.penalize(4);
-                            Ok(())
-                        }
+                        (a, b)
                     },
                     WhichPlayer::PlayerB => {
-                        Err(())
+                        (b, a)
                     },
+                };
+                let mut a = tuple.0;
+                let mut b = tuple.1;
+
+                if let Ok(booster) = Booster::from_str(&choice[..]) {
+                    if let Some(b_booster) = b.selected_booster {
+                        new_phase = Some(Phase::MoveChoosing(
+                            a.to_moveless_player(booster),
+                            b.to_moveless_player(b_booster),
+                        ));
+                        Ok(())
+                    } else {
+                        a.selected_booster = Some(booster);
+                        Ok(())
+                    }
+                } else {
+                    b.points += a.penalize(4);
+                    Ok(())
                 }
             },
 
             &mut Phase::MoveChoosing(ref mut a, ref mut b) => {
-                match chooser {
+                let tuple = match chooser {
                     WhichPlayer::PlayerA => {
-                        if let Ok(a_move) = Move::from_str(&choice[..]) {
-                            if a.available_moves().contains(&a_move) {
-                                if let Some(b_move) = b.selected_move {
-                                    let points = outcomes::get_points(vec![(a.booster, a_move), (b.booster, b_move)]);
-                                    a.points += points[0];
-                                    b.points += points[1];
-                                    a.move_streak.add(a_move);
-                                    b.move_streak.add(b_move);
-                                    a.selected_move = None;
-                                    b.selected_move = None;
-                                    Ok(())
-                                } else {
-                                    a.selected_move = Some(a_move);
-                                    Ok(())
-                                }
-                            } else {
-                                if a.destroyed_moves.contains(&a_move) {
-                                    b.points += a.penalize(4);
-                                    Ok(())
-                                } else if a.move_streak.times == 3
-                                    && a.move_streak.repeated_move == Some(a_move)
-                                {
-                                    b.points += a.penalize(3);
-                                    Ok(())
-                                } else {
-                                    let mut booster_moves = vec![];
-                                    for booster in &a.character.get_boosters() {
-                                        booster_moves.extend(booster.get_moves());
-                                    }
-
-                                    if booster_moves.contains(&a_move) {
-                                        b.points += a.penalize(2);
-                                        Ok(())
-                                    } else {
-                                        b.points += a.penalize(3);
-                                        Ok(())
-                                    }
-                                }
-                            }
-                        } else {
-                            b.points += a.penalize(4);
-                            Ok(())
-                        }
+                        (a, b)
                     },
                     WhichPlayer::PlayerB => {
-                        Err(())
+                        (b, a)
                     },
+                };
+                let mut a = tuple.0;
+                let mut b = tuple.1;
+
+                if let Ok(a_move) = Move::from_str(&choice[..]) {
+                    if a.available_moves().contains(&a_move) {
+                        if let Some(b_move) = b.selected_move {
+                            let points = outcomes::get_points(vec![(a.booster, a_move), (b.booster, b_move)]);
+                            a.points += points[0];
+                            b.points += points[1];
+                            a.move_streak.add(a_move);
+                            b.move_streak.add(b_move);
+                            a.selected_move = None;
+                            b.selected_move = None;
+                            Ok(())
+                        } else {
+                            a.selected_move = Some(a_move);
+                            Ok(())
+                        }
+                    } else {
+                        if a.destroyed_moves.contains(&a_move) {
+                            b.points += a.penalize(4);
+                            Ok(())
+                        } else if a.move_streak.times == 3
+                            && a.move_streak.repeated_move == Some(a_move)
+                        {
+                            b.points += a.penalize(3);
+                            Ok(())
+                        } else {
+                            let mut booster_moves = vec![];
+                            for booster in &a.character.get_boosters() {
+                                booster_moves.extend(booster.get_moves());
+                            }
+
+                            if booster_moves.contains(&a_move) {
+                                b.points += a.penalize(2);
+                                Ok(())
+                            } else {
+                                b.points += a.penalize(3);
+                                Ok(())
+                            }
+                        }
+                    }
+                } else {
+                    b.points += a.penalize(4);
+                    Ok(())
                 }
             },
-            _ => {
+
+            &mut Phase::GameOver(a_points, b_points) => {
+                // You can't make a move after the game is over.
                 Err(())
             },
         };
